7d62d707b51bbec79b23f9880e7b104df3c630c9
Alexander Anisimov <aliaksandr.anisimau@outlook.com>
2015-10-01 22:20:25 +0300

    refactoring: replace code by classes.pt2

--- web/Default.aspx.cs
+++ web/Default.aspx.cs
@@ -9,21 +9,21 @@ namespace FindAndFollow
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-          // loading page..
+            // loading page..
         }
 
         protected void BtnUploadData_Click(object sender, EventArgs e)
         {
             // INSERT Statement
             string insertStmt = "INSERT INTO dbo.UrlsTemp(OriginalURL) VALUES(NULLIF(@URL, 'null'))";
-            
+
             // Set up SQL Server connection
             string connStr = ConfigurationManager.ConnectionStrings["FindAndFollowConnectionString"].ConnectionString;
             SqlConnection sqlConnection = new SqlConnection(connStr);
-            
+
             sqlConnection.Open();
             SqlCommand commandInsert = new SqlCommand(insertStmt, sqlConnection);
-            
+
             // Define parameters
             commandInsert.Parameters.Add("@URL", SqlDbType.NVarChar, 1000);
 
@@ -33,7 +33,7 @@ namespace FindAndFollow
                 commandInsert.Parameters["@URL"].Value = Download.getData("http://www.av.by/public/public.php?event=View&public_id=" + i.ToString(), "/html[1]/body[1]/div[2]/div[1]/div[2]/div[1]/div[2]/header[1]/h1[1]", "CarName");
                 commandInsert.ExecuteNonQuery();
             }
-            
+
             // abw.by
             for (int i = 8155541; i < 8155561; i++)
             {
@@ -43,11 +43,11 @@ namespace FindAndFollow
 
             // ab.onliner.by
             for (int i = 2328800; i < 2328850; i++)
-			{
+            {
                 commandInsert.Parameters["@URL"].Value = Download.getData("http://ab.onliner.by/car/" + i.ToString(), "//*[@id='minWidth']/div/div[4]/div/div[2]/div[1]/div/ul/li/div/div/div/div[1]/p[1]/span[1]/strong", "CarName");
                 commandInsert.ExecuteNonQuery();
             }
-            
+
             sqlConnection.Close();
 
             // TODO add call stored procedure for clean "left" data
