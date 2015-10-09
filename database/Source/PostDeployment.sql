d4c0d5b011f7c7f49e9b07e38a5c40b3d05cb7ac
whitebeast <whitebeast@tut.by>
2015-10-09 10:30:31 +0300

    Added population of CurrencyRates table: during first deployment it populated by USD/EUR/RUB with current month rate values

--- database/Source/PostDeployment.sql
+++ database/Source/PostDeployment.sql
@@ -4,11 +4,32 @@ USE [$(msdb)]
 GO
 :r .\System\SQLJob\GetDailyCurrencyRates.sql
 GO
-
 USE [$(FindAndFollow)]
 GO
 EXEC dbo.CurrencyMerge
 GO
 
+USE [$(FindAndFollow)]
+GO
+DECLARE @DateBegin  DATE,
+	    @DateEnd    DATE = GETDATE()
+;	        
+SELECT  @DateBegin = DATEADD(DAY,1-DAY(@DateEnd),@DateEnd)
+;
+EXEC [CurrencyRateGet] 
+     @pCurrencyCode = 'USD',
+     @pFromDate     = @DateBegin,
+     @pToDate       = @DateEnd
+;
+EXEC [CurrencyRateGet] 
+     @pCurrencyCode = 'EUR',
+     @pFromDate     = @DateBegin,
+     @pToDate       = @DateEnd
+;
+EXEC [CurrencyRateGet] 
+     @pCurrencyCode = 'RUB',
+     @pFromDate     = @DateBegin,
+     @pToDate       = @DateEnd
+;               
 PRINT '---- End Post-Deployment script----'
 GO
