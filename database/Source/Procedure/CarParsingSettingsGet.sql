fe3a7ea3b3be899775ea8ef1811aae5adda74356
whitebeast <whitebeast@tut.by>
2015-10-13 11:50:51 +0300

    Task 26:Create procedure for get data from CarParsingSettings

---
+++ database/Source/Procedure/CarParsingSettingsGet.sql
@@ -0,0 +1,26 @@
+﻿CREATE PROCEDURE [dbo].[CarParsingSettingsGet]
+    (
+        @pSiteUrl   NVARCHAR(1000) 
+    )
+AS
+BEGIN
+    SELECT  CarBrandXPath, 
+            ModelXPath, 
+            ModelYearXPath, 
+            PriceXPath, 
+            MileageXPath, 
+            EngineSizeXPath,	
+            ColorXPath, 
+            BodyTypeXPath, 
+            EngineTypeXPath, 
+            TransmissionTypeXPath, 
+            DriveTypeXPath,		
+            DescriptionXPath, 
+            PageCreatedOnXPath, 
+            SellerTypeXPath, 
+            ConditionXPath, 
+            IsSwapXPath
+    FROM    dbo.CarParsingSettings
+    WHERE   SiteURL = @pSiteUrl
+    ;
+END
