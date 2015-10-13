346d22523787dca1642824bae36f837b497e6bb7
Alexander Anisimov <aliaksandr.anisimau@outlook.com>
2015-10-13 14:51:10 +0300

    Task 26: implemented procedure CarParsingSettingsGet on web service + minor refactoring

--- web/App_Code/Database.cs
+++ web/App_Code/Database.cs
@@ -24,33 +24,33 @@ namespace FindAndFollow
             sqlParameters[0] = new SqlParameter("nvarchar", SqlDbType.NVarChar, 4000);
             sqlParameters[0].Value = DBNull.Value;
 
-            string[] DataArray = new string[XPathArray.Length];
+            string[] dataArray = new string[XPathArray.Length];
 
             // Insert data
             for (int i = startId; i < finishId; i++)
             {
                 if (Download.IsPageExist(url + i.ToString()))
                 {
-                    DataArray = Download.GetData(url + i.ToString(), XPathArray, webSite);
-                    commandInsert.Parameters.Add("@pCarBrand", SqlDbType.NVarChar, 1000).Value = DataArray[0] ?? sqlParameters[0].Value;
-                    commandInsert.Parameters.Add("@pModel", SqlDbType.NVarChar, 1000).Value = DataArray[1] ?? sqlParameters[0].Value;
-                    commandInsert.Parameters.Add("@pModelYear", SqlDbType.NVarChar, 4000).Value = DataArray[2] ?? sqlParameters[0].Value;
-                    commandInsert.Parameters.Add("@pPrice", SqlDbType.NVarChar, 1000).Value = DataArray[3] ?? sqlParameters[0].Value;
-                    commandInsert.Parameters.Add("@pMileage", SqlDbType.NVarChar, 1000).Value = DataArray[4] ?? sqlParameters[0].Value;
-                    commandInsert.Parameters.Add("@pEngineSize", SqlDbType.NVarChar, 1000).Value = DataArray[5] ?? sqlParameters[0].Value;
-                    commandInsert.Parameters.Add("@pColor", SqlDbType.NVarChar, 1000).Value = DataArray[6] ?? sqlParameters[0].Value;
-                    commandInsert.Parameters.Add("@pBodyType", SqlDbType.NVarChar, 1000).Value = DataArray[7] ?? sqlParameters[0].Value;
-                    commandInsert.Parameters.Add("@pEngineType", SqlDbType.NVarChar, 1000).Value = DataArray[8] ?? sqlParameters[0].Value;
-                    commandInsert.Parameters.Add("@pTransmissionType", SqlDbType.NVarChar, 1000).Value = DataArray[9] ?? sqlParameters[0].Value;
-                    commandInsert.Parameters.Add("@pDriveType", SqlDbType.NVarChar, 1000).Value = DataArray[10] ?? sqlParameters[0].Value;
-                    commandInsert.Parameters.Add("@pDescription", SqlDbType.NVarChar, 4000).Value = DataArray[11] ?? sqlParameters[0].Value;
-                    commandInsert.Parameters.Add("@pPageCreatedOn", SqlDbType.NVarChar, 1000).Value = DataArray[12] ?? sqlParameters[0].Value;
+                    dataArray = Download.GetData(url + i.ToString(), XPathArray, webSite);
+                    commandInsert.Parameters.Add("@pCarBrand", SqlDbType.NVarChar, 1000).Value = dataArray[0] ?? sqlParameters[0].Value;
+                    commandInsert.Parameters.Add("@pModel", SqlDbType.NVarChar, 1000).Value = dataArray[1] ?? sqlParameters[0].Value;
+                    commandInsert.Parameters.Add("@pModelYear", SqlDbType.NVarChar, 4000).Value = dataArray[2] ?? sqlParameters[0].Value;
+                    commandInsert.Parameters.Add("@pPrice", SqlDbType.NVarChar, 1000).Value = dataArray[3] ?? sqlParameters[0].Value;
+                    commandInsert.Parameters.Add("@pMileage", SqlDbType.NVarChar, 1000).Value = dataArray[4] ?? sqlParameters[0].Value;
+                    commandInsert.Parameters.Add("@pEngineSize", SqlDbType.NVarChar, 1000).Value = dataArray[5] ?? sqlParameters[0].Value;
+                    commandInsert.Parameters.Add("@pColor", SqlDbType.NVarChar, 1000).Value = dataArray[6] ?? sqlParameters[0].Value;
+                    commandInsert.Parameters.Add("@pBodyType", SqlDbType.NVarChar, 1000).Value = dataArray[7] ?? sqlParameters[0].Value;
+                    commandInsert.Parameters.Add("@pEngineType", SqlDbType.NVarChar, 1000).Value = dataArray[8] ?? sqlParameters[0].Value;
+                    commandInsert.Parameters.Add("@pTransmissionType", SqlDbType.NVarChar, 1000).Value = dataArray[9] ?? sqlParameters[0].Value;
+                    commandInsert.Parameters.Add("@pDriveType", SqlDbType.NVarChar, 1000).Value = dataArray[10] ?? sqlParameters[0].Value;
+                    commandInsert.Parameters.Add("@pDescription", SqlDbType.NVarChar, 4000).Value = dataArray[11] ?? sqlParameters[0].Value;
+                    commandInsert.Parameters.Add("@pPageCreatedOn", SqlDbType.NVarChar, 1000).Value = dataArray[12] ?? sqlParameters[0].Value;
                     commandInsert.Parameters.Add("@pSiteUrl", SqlDbType.NVarChar, 4000).Value = url + i.ToString();
                     commandInsert.Parameters.Add("@pIsPageExist", SqlDbType.Bit).Value = true;
-                    commandInsert.Parameters.Add("@pSellerType", SqlDbType.NVarChar, 1000).Value = DataArray[13] == null ? "частное" : "автохаус";
-                    if (DataArray[14] != null)
+                    commandInsert.Parameters.Add("@pSellerType", SqlDbType.NVarChar, 1000).Value = dataArray[13] == null ? "частное" : "автохаус";
+                    if (dataArray[14] != null)
                     {
-                        if (DataArray[14].Contains("Кондиционер") || DataArray[14].Contains("климат-контроль"))
+                        if (dataArray[14].Contains("Кондиционер") || dataArray[14].Contains("климат-контроль"))
                             commandInsert.Parameters.Add("@pCondition", SqlDbType.NVarChar, 1000).Value = "1";
                         else
                             commandInsert.Parameters.Add("@pCondition", SqlDbType.NVarChar, 1000).Value = "0";
@@ -59,7 +59,7 @@ namespace FindAndFollow
                     {
                         commandInsert.Parameters.Add("@pCondition", SqlDbType.NVarChar, 1000).Value = "0";
                     }
-                    commandInsert.Parameters.Add("@pIsSwap", SqlDbType.NVarChar, 1000).Value = DataArray[15] == null ? "0" : "1";
+                    commandInsert.Parameters.Add("@pIsSwap", SqlDbType.NVarChar, 1000).Value = dataArray[15] == null ? "0" : "1";
 
                     commandInsert.ExecuteNonQuery();
                 }
@@ -107,46 +107,42 @@ namespace FindAndFollow
 
         public static string[] GetCarParsingSettings(string Url)
         {
-            string[] XPathArray = new string[16];
+            string[] xPathArray = new string[16];
             
             string connStr = ConfigurationManager.ConnectionStrings["FindAndFollowConnectionString"].ConnectionString;
             SqlConnection sqlConnection = new SqlConnection(connStr);
 
-            string selectString = "SELECT CarBrandXPath, ModelXPath, ModelYearXPath, PriceXPath, MileageXPath, EngineSizeXPath," + 
-                                    "ColorXPath, BodyTypeXPath, EngineTypeXPath, TransmissionTypeXPath, DriveTypeXPath," + 
-                                    "DescriptionXPath, PageCreatedOnXPath, SellerTypeXPath, ConditionXPath, IsSwapXPath " +
-                                    "FROM CarParsingSettings WHERE SiteUrlXPath = @SiteUrl";
-
-            SqlCommand commandSelect = new SqlCommand(selectString, sqlConnection);
-            commandSelect.Parameters.AddWithValue("@SiteUrl", Url);
+            SqlCommand commandSelect = new SqlCommand("CarParsingSettingsGet", sqlConnection);
+            commandSelect.CommandType = CommandType.StoredProcedure;
+            commandSelect.Parameters.AddWithValue("@pSiteUrl", Url);
 
             sqlConnection.Open();
 
             // Get data from db
-            SqlDataReader DataReader = commandSelect.ExecuteReader();
-            while (DataReader.Read())
+            SqlDataReader dataReader = commandSelect.ExecuteReader();
+            while (dataReader.Read())
             {
-                XPathArray[0] = DataReader["CarBrandXPath"].ToString();
-                XPathArray[1] = DataReader["ModelXPath"].ToString();
-                XPathArray[2] = DataReader["ModelYearXPath"].ToString();
-                XPathArray[3] = DataReader["PriceXPath"].ToString();
-                XPathArray[4] = DataReader["MileageXPath"].ToString();
-                XPathArray[5] = DataReader["EngineSizeXPath"].ToString();
-                XPathArray[6] = DataReader["ColorXPath"].ToString();
-                XPathArray[7] = DataReader["BodyTypeXPath"].ToString();
-                XPathArray[8] = DataReader["EngineTypeXPath"].ToString();
-                XPathArray[9] = DataReader["TransmissionTypeXPath"].ToString();
-                XPathArray[10] = DataReader["DriveTypeXPath"].ToString();
-                XPathArray[11] = DataReader["DescriptionXPath"].ToString();
-                XPathArray[12] = DataReader["PageCreatedOnXPath"].ToString();
-                XPathArray[13] = DataReader["PageCreatedOnXPath"].ToString();
-                XPathArray[14] = DataReader["ConditionXPath"].ToString();
-                XPathArray[15] = DataReader["IsSwapXPath"].ToString();
+                xPathArray[0] = dataReader["CarBrandXPath"].ToString();
+                xPathArray[1] = dataReader["ModelXPath"].ToString();
+                xPathArray[2] = dataReader["ModelYearXPath"].ToString();
+                xPathArray[3] = dataReader["PriceXPath"].ToString();
+                xPathArray[4] = dataReader["MileageXPath"].ToString();
+                xPathArray[5] = dataReader["EngineSizeXPath"].ToString();
+                xPathArray[6] = dataReader["ColorXPath"].ToString();
+                xPathArray[7] = dataReader["BodyTypeXPath"].ToString();
+                xPathArray[8] = dataReader["EngineTypeXPath"].ToString();
+                xPathArray[9] = dataReader["TransmissionTypeXPath"].ToString();
+                xPathArray[10] = dataReader["DriveTypeXPath"].ToString();
+                xPathArray[11] = dataReader["DescriptionXPath"].ToString();
+                xPathArray[12] = dataReader["PageCreatedOnXPath"].ToString();
+                xPathArray[13] = dataReader["PageCreatedOnXPath"].ToString();
+                xPathArray[14] = dataReader["ConditionXPath"].ToString();
+                xPathArray[15] = dataReader["IsSwapXPath"].ToString();
             }
 
             sqlConnection.Close();
 
-            return XPathArray;
+            return xPathArray;
         }
     }
 }
