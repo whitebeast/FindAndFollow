735ff9f3e4f60e25c97c853f75bed9f843c2a5b5
whitebeast <whitebeast@tut.by>
2015-11-07 16:54:54 +0300

    Bug 52 - db project failed after commit for task 38

--- database/Source/PreDeployment.sql
+++ database/Source/PreDeployment.sql
@@ -1,4 +1,6 @@
-﻿sp_configure 'show advanced options', 1;
+﻿PRINT '---- Start Pre-Deployment script----'
+GO
+sp_configure 'show advanced options', 1;
 GO
 RECONFIGURE;
 GO
@@ -10,31 +12,7 @@ sp_configure 'Database Mail XPs', 1
 GO
 RECONFIGURE;
 GO
-EXECUTE msdb.dbo.sysmail_add_account_sp
-		@account_name = 'FindAndFollow.Notification@gmail.com',
-		@description = N'Notification account for FindAndFollow project',
-		@email_address = 'FindAndFollow.Notification@gmail.com',
-		@display_name = N'Notification service',
-		@replyto_address = 'no-reply@gmail.com',
-		@mailserver_name = 'smtp.gmail.com',
-		@port = 465,
-		@username = 'FindAndFollow.Notification@gmail.com',
-		@password = 'VsKexibt!',
-		@enable_ssl = 1;
-EXECUTE msdb.dbo.sysmail_add_profile_sp
-		@profile_name = 'FindAndFollow.Notification Mailer';
-EXECUTE msdb.dbo.sysmail_add_profileaccount_sp
-		@profile_name = 'FindAndFollow.Notification Mailer',
-		@account_name = 'FindAndFollow.Notification@gmail.com',
-		@sequence_number = 1;
-EXECUTE msdb.dbo.sysmail_add_principalprofile_sp
-		@profile_name = 'FindAndFollow.Notification Mailer',
-		@principal_id = 0,
-		@is_default = 1;
-USE [msdb]
+:r .\Script\PreDeployment\EmailProfile.sql
 GO
-EXEC msdb.dbo.sp_add_operator @name=N'Administrator', 
-		@enabled=1, 
-		@pager_days=0, 
-		@email_address=N'aliaksandr.anisimau@outlook.com;whitebeast@tut.by'
+PRINT '---- End Pre-Deployment script----'
 GO
---
+++ database/Source/Script/PreDeployment/EmailProfile.sql
@@ -0,0 +1,64 @@
+﻿DECLARE @AccountName VARCHAR(100) = 'FindAndFollow.Notification@gmail.com',
+		@ProfileName VARCHAR(100) = 'FindAndFollow.Notification Mailer';
+		
+IF NOT EXISTS (SELECT 1 FROM msdb.dbo.sysmail_account AS sa WHERE NAME = @AccountName) BEGIN 
+	PRINT 'Configuring Email account';
+	EXECUTE msdb.dbo.sysmail_add_account_sp
+			@account_name = @AccountName,
+			@description = N'Notification account for FindAndFollow project',
+			@email_address = @AccountName,
+			@display_name = N'Notification service',
+			@replyto_address = 'no-reply@gmail.com',
+			@mailserver_name = 'smtp.gmail.com',
+			@port = 587,
+			@username = @AccountName,
+			@password = 'VsKexibt!',
+			@enable_ssl = 1;
+END;	
+
+IF NOT EXISTS (SELECT 1 FROM msdb.dbo.sysmail_profile AS sp WHERE NAME = @ProfileName) BEGIN
+	PRINT 'Configuring Email profile';	
+	EXECUTE msdb.dbo.sysmail_add_profile_sp
+			@profile_name = @ProfileName
+	EXECUTE msdb.dbo.sysmail_add_profileaccount_sp
+			@profile_name = @ProfileName,
+			@account_name = @AccountName,
+			@sequence_number = 1;
+	EXECUTE msdb.dbo.sysmail_add_principalprofile_sp
+			@profile_name = @ProfileName,
+			@principal_id = 0,
+			@is_default = 1;
+END;
+
+IF NOT EXISTS (SELECT 1 FROM msdb.dbo.sysoperators AS s WHERE s.name = 'Administrator') BEGIN
+	PRINT 'Configuring Email operator'
+	EXEC msdb.dbo.sp_add_operator @name=N'Administrator', 
+			@enabled=1, 
+			@pager_days=0, 
+			@email_address=N'aliaksandr.anisimau@outlook.com;whitebeast@tut.by'
+END;	
+		
+/*
+DECLARE @AccountName VARCHAR(100) = 'FindAndFollow.Notification@gmail.com',
+		@ProfileName VARCHAR(100) = 'FindAndFollow.Notification Mailer';
+
+IF EXISTS (SELECT 1 FROM msdb.dbo.sysmail_account AS sa WHERE NAME = @AccountName) BEGIN
+	PRINT 'Deleting Email account';                                                                                   	
+	EXEC msdb.dbo.sysmail_delete_account_sp 
+		@account_name = @AccountName
+END;
+
+IF EXISTS (SELECT * FROM msdb.dbo.sysmail_profile AS sp WHERE NAME = @ProfileName) BEGIN
+	PRINT 'Deleting Email profile';
+	EXEC msdb.dbo.sysmail_delete_profileaccount_sp 
+		@profile_name = @ProfileName
+		
+	EXEC msdb.dbo.sysmail_delete_profile_sp
+		@profile_name = @ProfileName
+END;	
+
+IF EXISTS (SELECT 1 FROM msdb.dbo.sysoperators AS s WHERE s.name = 'Administrator') BEGIN
+	PRINT 'Deleting Email operator'
+	EXEC msdb.dbo.sp_delete_operator @name = 'Administrator' ;
+END;
+ */
