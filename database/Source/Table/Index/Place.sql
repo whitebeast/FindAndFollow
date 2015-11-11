b2fac2803bc279bce7e8569ca0f0e331d8deeda5
whitebeast <whitebeast@tut.by>
2015-11-11 12:11:05 +0300

    Task 29:Configure indexes on tables

--- database/Source/Table/Index/Place.sql
+++ database/Source/Table/Index/Place.sql
@@ -1,4 +1,6 @@
-﻿CREATE NONCLUSTERED INDEX [ixCurrency_CharCode] ON [dbo].[Currency]
+﻿CREATE UNIQUE NONCLUSTERED INDEX [ixPlace_CityId_CountryId_RegionId] ON [dbo].[Place]
 (
-    [CharCode] ASC
+    [CityId] ASC,
+    [CountryId] ASC,
+    [RegionId] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = ON, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
--- database/Source/Table/Site.sql
+++ database/Source/Table/Site.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [Site]
     (
         SiteId               INT IDENTITY(1, 1) NOT NULL,
-        SiteUrl              VARCHAR(100) NOT NULL,
+        SiteUrl              NVARCHAR(100) NOT NULL,
         ShortDescription     NVARCHAR(100) NOT NULL,
         FullDescription      NVARCHAR(500) NOT NULL,
         CountryId            INT NOT NULL,
