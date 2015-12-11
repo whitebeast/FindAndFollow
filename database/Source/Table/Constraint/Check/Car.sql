3a5df88e87078504b67684b63acd3e7a255ec764
whitebeast <whitebeast@tut.by>
2015-12-11 15:18:39 +0300

    Refactoring: fixed check constraints

--- database/Source/Table/Constraint/Check/Car.sql
+++ database/Source/Table/Constraint/Check/Car.sql
@@ -1,23 +1,23 @@
 ﻿ALTER TABLE dbo.Car
-ADD CONSTRAINT ckCar_BodyType CHECK (BodyType >= 1 AND BodyType <= 12);
+ADD CONSTRAINT ckCar_BodyType CHECK (BodyType >= 0 AND BodyType <= 12);
 GO
 
 ALTER TABLE dbo.Car
-ADD CONSTRAINT ckCar_EngineType CHECK (EngineType >= 1 AND EngineType <= 6);
+ADD CONSTRAINT ckCar_EngineType CHECK (EngineType >= 0 AND EngineType <= 6);
 GO
 
 ALTER TABLE dbo.Car
-ADD CONSTRAINT ckCar_TransmissionType CHECK (TransmissionType >= 1 AND TransmissionType <= 2);
+ADD CONSTRAINT ckCar_TransmissionType CHECK (TransmissionType >= 0 AND TransmissionType <= 2);
 GO
 
 ALTER TABLE dbo.Car
-ADD CONSTRAINT ckCar_DriveType CHECK (DriveType >= 1 AND DriveType <= 3);
+ADD CONSTRAINT ckCar_DriveType CHECK (DriveType >= 0 AND DriveType <= 3);
 GO
 
 ALTER TABLE dbo.Car
-ADD CONSTRAINT ckCar_Condition CHECK (Condition >= 1 AND Condition <= 3);
+ADD CONSTRAINT ckCar_Condition CHECK (Condition >= 0 AND Condition <= 3);
 GO
 
 ALTER TABLE dbo.Car
-ADD CONSTRAINT ckCar_SellerType CHECK (SellerType >= 1 AND SellerType <= 3);
+ADD CONSTRAINT ckCar_SellerType CHECK (SellerType >= 0 AND SellerType <= 3);
 GO
