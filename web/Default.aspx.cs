36c05e33a3ca3c201a0a971ea317d635fff6c2cf
Alexander Anisimov <aliaksandr.anisimau@outlook.com>
2015-12-12 14:03:03 +0200

    Task 95: implemented threads on service

--- web/Default.aspx.cs
+++ web/Default.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace FindAndFollow
 {
@@ -11,37 +12,53 @@ namespace FindAndFollow
 
         protected void BtnUploadData_Click(object sender, EventArgs e)
         {
+            Thread threadAbw = new Thread(AbwThread);
+            threadAbw.Start();
+
+            Thread threadAb = new Thread(AbThread);
+            threadAb.Start(); 
+
             string siteAv = "av.by";
-            string siteAbwP = "abw.by-private";
-            string siteAbwA = "abw.by-autoagency";
-            string siteAb = "ab.onliner.by";
 
             string urlAv = Database.CarParsingSettingsDownloadMaskUrl(siteAv);
-            string urlAbw = Database.CarParsingSettingsDownloadMaskUrl(siteAbwP);
-            string urlAb = Database.CarParsingSettingsDownloadMaskUrl(siteAb);
 
-            // av.by
             string[] mainPageXpaths = Database.CarParsingSettingsMainPage(siteAv);
             int[] urlIds = Download.UrlsGet(mainPageXpaths[0], siteAv, mainPageXpaths[1], mainPageXpaths[2], mainPageXpaths[3]);
 
             string[] xPathArray = Database.CarParsingSettingsGet(siteAv);
             Database.CarParsingInsert(urlAv, xPathArray, urlIds, mainPageXpaths[4], siteAv);
 
-            // abw.by
-            mainPageXpaths = Database.CarParsingSettingsMainPage(siteAbwA);
-            urlIds = Download.UrlsGet(mainPageXpaths[0], siteAbwA, mainPageXpaths[1], mainPageXpaths[2], mainPageXpaths[3]);
+            Database.CarMerge();
+        }
+
+        static void AbwThread()
+        {
+            string siteAbwP = "abw.by-private";
+            string siteAbwA = "abw.by-autoagency";
+
+            string urlAbw = Database.CarParsingSettingsDownloadMaskUrl(siteAbwP);
+            string[] xPathArray = Database.CarParsingSettingsGet(siteAbwP);
+
+            string[] mainPageXpaths = Database.CarParsingSettingsMainPage(siteAbwA);
+            int[] urlIds = Download.UrlsGet(mainPageXpaths[0], siteAbwA, mainPageXpaths[1], mainPageXpaths[2], mainPageXpaths[3]);
 
             Database.CarParsingInsert(urlAbw, xPathArray, urlIds, mainPageXpaths[4], "abw.by");
+        }
 
-            // ab.onliner.by
-            xPathArray = Database.CarParsingSettingsGet(siteAb);
+        static void AbThread()
+        {
+            string siteAb = "ab.onliner.by";
+            string siteAbwA = "abw.by-autoagency";
+
+            string urlAb = Database.CarParsingSettingsDownloadMaskUrl(siteAb);
+
+            string[] mainPageXpaths = Database.CarParsingSettingsMainPage(siteAbwA);
+            string[] xPathArray = Database.CarParsingSettingsGet(siteAb);
 
             //int[] urlIds = Download.UrlsGet(urlSiteAb, "//*[@id=\"minWidth\"]/div/div[4]/div/div[2]/div[1]/div/div[2]/div[2]/table/tbody", siteAb, ".//div[@class='b-listing-item-title']//a[@href]", "href", "public.php?event=View&public_id=");
-            urlIds = new int[] { 2463032, 2441437, 2441221, 2395719 };
+            int[] urlIds = new int[] { 2478464, 2478462, 2478461, 2478457, 2478453, 2478452 };
 
             Database.CarParsingInsert(urlAb, xPathArray, urlIds, mainPageXpaths[4], siteAb);
-
-            Database.CarMerge();
         }
     }
 }
